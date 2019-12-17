7fb1fbd22db5e3f1b208eef96b452ef0e4c2906f
DeltaByte <thedeltabyte@gmail.com>
2019-12-17 00:32:57 +0000

    WIP: add remote state

---
+++ remote-state.tf
@@ -0,0 +1,122 @@
+locals {
+  bucket_name = "mediacodex-terraform-state-dev"
+  dynamo_name = "mediacodex-terraform-lock-dev"
+  region = "eu-west-2" //TODO: add replication
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = local.bucket_name
+  acl    = "private"
+  policy = data.aws_iam_policy_document.prevent_unencrypted_uploads.*.json
+
+  versioning = {
+    enabled = true
+    mfa_delete = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = var.default_tags
+}
+
+data "aws_iam_policy_document" "prevent_unencrypted_uploads" {
+  count = local.prevent_unencrypted_uploads ? 1 : 0
+
+  statement {
+    sid = "DenyIncorrectEncryptionHeader"
+
+    effect = "Deny"
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}/*",
+    ]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+
+      values = [
+        "AES256",
+      ]
+    }
+  }
+
+  statement {
+    sid = "DenyUnEncryptedObjectUploads"
+
+    effect = "Deny"
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}/*",
+    ]
+
+    condition {
+      test     = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+
+      values = [
+        "true",
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket                  = aws_s3_bucket.default.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  count          = var.enable_server_side_encryption ? 1 : 0
+  name           = local.dynamo_name
+  read_capacity  = 5
+  write_capacity = 5
+
+  # https://www.terraform.io/docs/backends/types/s3.html#dynamodb_table
+  hash_key = "LockID"
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      read_capacity,
+      write_capacity
+    ]
+  }
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = var.default_tags
+}
--- terraform.tf
+++ terraform.tf
@@ -3,12 +3,21 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "mediacodex-terraform-state-dev"
+    key    = "infrastructure"
+    region = "eu-west-2"
+    dynamodb_table = "mediacodex-terraform-lock-dev"
+  }
+}
+
 variable "default_tags" {
-  type        = map(string)
+  type        = "map"
   description = "Common resource tags for all resources"
   default = {
     Application = "MediaCodex"
+    Service = "Infrastructure"
   }
   # TODO: add stage
 }
-
