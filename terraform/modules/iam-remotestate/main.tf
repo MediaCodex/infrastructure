f869485e08c1ccbc63f7781be3b1e6da6e7e97f7
DeltaByte <thedeltabyte@gmail.com>
2020-04-05 01:13:28 +0100

    Create module for remotestate assumable roles

---
+++ terraform/modules/iam-remotestate/main.tf
@@ -0,0 +1,57 @@
+/**
+ * Assumable Role
+ */
+resource "aws_iam_role" "remotestate" {
+  path = "/remotestate/"
+  name               = var.service
+  description        = "Deployment role for '${var.service}' service"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.user]
+    }
+  }
+}
+
+/**
+ * Access policy
+ */
+resource "aws_iam_role_policy" "terraform_state" {
+  name   = "TerraformRemoteState"
+  role   = aws_iam_role.remotestate.id
+  policy = data.aws_iam_policy_document.terraform_state.json
+}
+data "aws_iam_policy_document" "terraform_state" {
+  statement {
+    sid = "S3State"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:PutObject"
+    ]
+    resources = ["${local.bucket}/env:/${var.workspace}/${var.service}.tfstate"]
+  }
+
+    statement {
+    sid = "S3ListObjects"
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [local.bucket]
+  }
+
+  statement {
+    sid = "DynamoLock"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = [local.table]
+  }
+}
---
+++ terraform/modules/iam-remotestate/variables.tf
@@ -0,0 +1,19 @@
+locals {
+  bucket = "arn:aws:s3:::terraform-state-mediacodex"
+  table  = "arn:aws:dynamodb:eu-central-1:939514526661:table/terraform-state-lock"
+}
+
+variable "user" {
+  type = string
+  description = "ARN of the IAM deploy user"
+}
+
+variable "service" {
+  type = string
+  description = "Service name"
+}
+
+variable "workspace" {
+  type = string
+  description = "Terrafrom workspace"
+}
