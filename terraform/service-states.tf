03a0407f111554f27f5e60e7ca9ba3c15712c536
DeltaByte <thedeltabyte@gmail.com>
2020-06-07 05:15:16 +0100

    add core service remotestate

--- terraform/service-states.tf
+++ terraform/service-states.tf
@@ -7,3 +7,13 @@ module "remotestate_aws_access" {
   service   = "aws-access"
   user      = "arn:aws:iam::949257948165:user/deployment/deploy-aws-access"
 }
+
+/**
+ * Core
+ */
+module "remotestate_core" {
+  source    = "./modules/iam-remotestate"
+  workspace = "development"
+  service   = "core"
+  user      = "arn:aws:iam::949257948165:user/deployment/deploy-core"
+}
